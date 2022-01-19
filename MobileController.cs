72ff6ec72aebd0601883d5af6e196abcaa26b9a8
Alexandre Malaquin <35108299+Monfred156@users.noreply.github.com>
2022-01-20 01:13:55 +0900

    Create MobileController.cs

---
+++ MobileController.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileController : MonoBehaviour
+{
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private Vector2 startTouch, swipeDelta;
+    private bool isDraging = false;
+
+    private bool countTrigger = true;
+
+    private Vector3 positionToMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionToMove = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+        if (Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                tap = true;
+                isDraging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                Reset();
+            }
+        }
+        swipeDelta = Vector2.zero;
+        if (isDraging)
+        {
+            if (Input.touches.Length > 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+        }
+
+        if (swipeDelta.magnitude > 10)
+        {
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+
+            if (Math.Abs(x) > Math.Abs(y))
+            {
+                if (x < 0)
+                {
+                    swipeLeft = true;
+                }
+                else
+                {
+                    swipeRight = true;
+                }
+            }
+            else
+            {
+                if (y < 0)
+                {
+                    swipeDown = true;
+                }
+                else
+                {
+                    swipeUp = true;
+                }
+            }
+            Reset();
+        }
+
+        if (swipeLeft && transform.position.x > -2.5)
+        {
+            positionToMove = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        } else if (swipeRight && transform.position.x < 2.5)
+        {
+            positionToMove = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        } else if (swipeUp && transform.position.y < 4.5)
+        {
+            positionToMove = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        } else if (swipeDown && transform.position.y > -4.5)
+        {
+            positionToMove = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, positionToMove, 25 * Time.deltaTime);
+
+    }
+
+    private void Reset()
+    {
+        startTouch = Vector2.zero;
+        swipeDelta = Vector2.zero;
+        isDraging = false;
+    }
+}
